a9624067329f9719a6636e32df59d93d95a3e631
mikaelmonteirodev <mikaelmonteiro@gmail.com>
2023-05-20 14:50:54 -0300

    feat: create button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,6 @@
-import React from 'react';
-import './button.css';
+
 
 interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
   /**
    * What background color to use
    */
@@ -28,17 +23,14 @@ interface ButtonProps {
  * Primary UI component for user interaction
  */
 export const Button = ({
-  primary = false,
   size = 'medium',
   backgroundColor,
   label,
   ...props
 }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
     <button
       type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
       style={{ backgroundColor }}
       {...props}
     >
