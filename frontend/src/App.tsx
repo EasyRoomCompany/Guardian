e85dc5ac822bfde06eec026ef0097fca8973658f
Joel Lacerda <joellacerdaol@gmail.com>
2023-05-21 12:16:08 -0300

    update: tailwind configs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,8 @@ function App() {
       <Image
         src="https://mundoconectado.com.br/uploads/2022/05/25/25658/cacto.jpg"
         alt="cactus"
+        width="w-1/5"
+        height="h-1/5"
       />
     </>
   );
