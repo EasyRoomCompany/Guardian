4bb731e195b7782a20ca9cefe1b0acb1f8442c72
Joel Lacerda <joellacerdaol@gmail.com>
2023-05-21 12:25:03 -0300

    update: reset to default

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,5 @@
-import { Button } from "./components/Button";
-import { Image } from "./components/Image";
-
 function App() {
-  return (
-    <>
-      <Button label="Hello"></Button>
-      <Image
-        src="https://mundoconectado.com.br/uploads/2022/05/25/25658/cacto.jpg"
-        alt="cactus"
-        width="w-1/5"
-        height="h-1/5"
-      />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
