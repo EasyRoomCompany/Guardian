37d3311b9a2398e8fa307d7dc2af2caa19b61194
Joel Lacerda <joellacerdaol@gmail.com>
2023-05-21 11:58:16 -0300

    update: tailwind configs

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./frontend/**/*.{html,js,jsx,ts,tsx}",
-    "./src/**/*.{html,js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
 
   theme: {
     extend: {},
