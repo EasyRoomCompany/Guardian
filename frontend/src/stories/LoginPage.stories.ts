df708ea94b6ce17801f0e325f590455fc3bd2124
kevin <kevindiegodasilvasousa@gmail.com>
2023-05-21 19:44:38 -0300

    fix: Correcting comments loginpage

--- frontend/src/stories/LoginPage.stories.ts
+++ frontend/src/stories/LoginPage.stories.ts
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { LoginPage } from '../pages/LoginPage';
 
 /**
- * The input component is capable of adapting itself according to its type, and its entire tailwind class can be implemented or left only in the default
+ * Component of the login page to access the application, optional dakmode for the user.
  */
 const meta = {
     title: 'Pages/LoginPage',
@@ -13,9 +13,6 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-/**
- * replace the alert for implementation in dakmode
- */
 export const ThemeToggleButton_component: Story = {
     args: {
         theme: 'dark',
