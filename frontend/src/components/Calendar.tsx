ac69737534f55571525964b57b57e5305201ab70
Joel Lacerda <joellacerdaol@gmail.com>
2023-05-24 13:07:43 -0300

    fix: calendar and easyroompage import

--- frontend/src/components/Calendar.tsx
+++ frontend/src/components/Calendar.tsx
@@ -1,21 +1,21 @@
-import * as React from 'react';
-import dayjs, { Dayjs } from 'dayjs';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import * as React from "react";
+import dayjs, { Dayjs } from "dayjs";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
-export default function Calendar() {
-  const [value, setValue] = React.useState<Dayjs | null>(dayjs('2022-04-17'));
+export function Calendar() {
+  const [value, setValue] = React.useState<Dayjs | null>(dayjs("2022-04-17"));
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DemoContainer components={['DatePicker', 'DatePicker']}>
+      <DemoContainer components={["DatePicker", "DatePicker"]}>
         <DatePicker
           label="Appointment Date"
           value={value}
           onChange={(newValue) => setValue(newValue)}
-          className='appearance-none rounded-none relative block   px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-orange-500 focus:border-orange-500 focus:z-10 sm:text-sm'
+          className="appearance-none rounded-none relative block   px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-orange-500 focus:border-orange-500 focus:z-10 sm:text-sm"
         />
       </DemoContainer>
     </LocalizationProvider>
