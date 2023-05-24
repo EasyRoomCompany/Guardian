fb82ed32efe383341bb9151c505d78d279d65833
kevin <kevindiegodasilvasousa@gmail.com>
2023-05-24 17:58:00 -0300

    bug: Resolve easy interface

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const PORT = 9000;
+const PORT = 3333;
 const pool = new Pool({
   connectionString: process.env.POSTGRESS_URL,
 });
